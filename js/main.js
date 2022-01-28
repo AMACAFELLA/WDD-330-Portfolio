9659cb9b3d450f9cb3899ab69a883d2aa9a9fcac
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-01-28 14:07:24 +0200

    Links updated

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    },
-    {
-      label: "Week2 notes",
-      url: "/week2/index.html"
-    },
-    {
-      label: "Week3 notes",
-      url: "/week3/index.html"
-    },
-    {
-      label: "Week4 notes",
-      url: "/week4/index.html"
-    }
-  ]
-  links.forEach(link => {
-    const a = document.createElement("a");
-    a.href = link.url;
-    a.textContent = link.label;
-    document.body.appendChild(a);
-  });
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week2 notes",
+    url: "/week2/index.html"
+  },
+  {
+    label: "Week3 notes",
+    url: "/week3/index.html"
+  },
+  {
+    label: "Week4 notes",
+    url: "/week4/index.html"
+  }
+]
+let linksList = document.getElementById("linksList");
+
+for(let i = 0; i < links.length; i++) {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  let label = document.createTextNode(links[i].label);
+  a.href = links[i].url;
+  a.appendChild(label);
+  li.appendChild(a);
+  linksList.appendChild(li);
+}
