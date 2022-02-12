b2e13720acca623d029c1bdd73c21607b75f9ef6
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-02-12 10:48:22 +0200

    Update Again

--- js/main.js
+++ js/main.js
@@ -20,21 +20,16 @@ const links = [
     url: "/week5/index.html",
   }
 ];
+ 
 let linksList = document.getElementById("linksList");
-//label and url into a link and add it to the list
-for (let i = 0; i < links.length; i++) {
-  let link = document.createElement("a");
-  link.href = links[i].url;
-  link.innerHTML = links[i].title;
-  linksList.appendChild(link);
-}
 
-/*for(let i = 0; i < links.length; i++) {
+
+for(let i = 0; i < links.length; i++) {
   let li = document.createElement("li");
   let a = document.createElement("a");
-  let label = document.createTextNode(links[i].label);
+  let label = document.createTextNode(links[i].title);
   a.href = links[i].url;
   a.appendChild(label);
   li.appendChild(a);
   linksList.appendChild(li);
-}*/
+}
