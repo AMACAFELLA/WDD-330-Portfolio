6416e3c2add30ed3759b493730a0212ad39e8547
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-02-12 10:51:12 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const links = [
   {
-    title: "Week 1",
+    label: "Week 1",
     url: "/week1/index.html",
   },
   {
-    title: "Week 2",
+    label: "Week 2",
     url: "/week2/index.html",
   },
   {
-    title: "week 3",
+    label: "week 3",
     url: "/week3/index.html",
   },
   {
-    title: "week 4",
+    label: "week 4",
     url: "/week4/index.html",
   },
   {
-    title: "week 5",
+    label: "week 5",
     url: "/week5/index.html",
   }
 ];
@@ -27,7 +27,7 @@ let linksList = document.getElementById("linksList");
 for(let i = 0; i < links.length; i++) {
   let li = document.createElement("li");
   let a = document.createElement("a");
-  let label = document.createTextNode(links[i].title);
+  let label = document.createTextNode(links[i].label);
   a.href = links[i].url;
   a.appendChild(label);
   li.appendChild(a);
