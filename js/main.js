c17be28cbb57d7b903edb6380a1429fcbbb065e3
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-02-12 10:34:09 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const links = [
     label: "Week1 notes",
     url: "week1/index.html",
     label: "Week2 notes",
-<<<<<<< HEAD
     url: "./week2/index.html",
     label: "Week3 notes",
     url: "/week3/index.html",
@@ -11,17 +10,6 @@ const links = [
     url: "/week4/index.html",
     label: "Week5 notes",
     url: "/week5/index.html"
-=======
-    url: "./week2/index.html"
-  },
-  {
-    label: "Week3 notes",
-    url: "./week3/index.html"
-  },
-  {
-    label: "Week4 notes",
-    url: "./week4/index.html"
->>>>>>> 1c20e6cef300818add69f0248aef30cb82dff6a2
   },
   {
     label: "Week1 notes",
@@ -33,15 +21,8 @@ const links = [
     label: "Week4 notes",
     url: "/week4/index.html",
     label: "Week5 notes",
-<<<<<<< HEAD
     url: "/week5/index.html"
   }];
-=======
-    url: "./week5/index.html"
-  }
-
-]
->>>>>>> 1c20e6cef300818add69f0248aef30cb82dff6a2
 let linksList = document.getElementById("linksList");
 //make each label and url into a link and add it to the list
 for (let i = 0; i < links.length; i++) {
